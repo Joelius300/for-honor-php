6dc4f0bc00339bcf0210091e03debe7beee76939
Seger Nik <bsegen@bbc-be.local>
2018-12-10 15:35:18 +0100

    sign up view edited

--- views/user_create.php
+++ views/user_create.php
@@ -1,38 +1,32 @@
 <form data-toggle="validator" class="form-horizontal" action="/user/Register" method="post">
 	<div class="component" data-html="true">
-		<div class="form-group">
-		  <label class="col-md-2 control-label" for="textinput">Benutzername</label>
-		  <div class="col-md-4">
-		  	<input id="username" name="username" type="text" class="form-control input-md" maxlength='30' placeholder='Benutzername' required>
+		<img class="login-pic" src="/images/main-symbol.jpg" width="200px" height="200px">
+		<div>
+			<div class="center">
+				<label class="control-label" for="textinput">Benutzername</label>
+			</div>
+		  <div class="center">
+		  	<input id="username" name="username" type="text" class="form-control input-md center" maxlength='30' placeholder='Benutzername' required>
 		  </div>
 		</div>
-		<!-- <div class="form-group">
-		  <label class="col-md-2 control-label" for="textinput">Passwort</label>
-		  <div class="col-md-4">
-		  	<input id="password" name="password" type="password" class="form-control input-md">
-		  </div>
-		</div>
-		<div class="form-group">
-		  <label class="col-md-2 control-label" for="textinput">Passwort bestätigen</label>
-		  <div class="col-md-4">
-		  	<input id="passwordConfirm" name="passwordConfirm" type="password" class="form-control input-md">
-		  </div>
-		</div> -->
 
-		<div class="form-group">
-			<label class="col-md-2 control-label" for="textinput">Passwort</label>
-    	<div class="col-md-4">
-        <input name='password' type="password" data-minlength="6" class="form-control" id="inputPassword" placeholder="Passwort" required>
+		<div>
+			<div class="center">
+				<label class="control-label" for="textinput">Passwort</label>
+			</div>
+    	<div class="center">
+        <input name='password' type="password" data-minlength="6" class="form-control center" id="inputPassword" placeholder="Passwort" required>
         <div class="help-block">Minimum of 6 characters</div>
-				<input type="password" class="form-control" id="inputPasswordConfirm" data-match="#inputPassword" placeholder="Password bestätigen" required>
+				<input type="password" class="form-control center" id="inputPasswordConfirm" data-match="#inputPassword" placeholder="Password bestätigen" required>
       </div>
     </div>
 
-		<div class="form-group">
-	      <label class="col-md-2 control-label" for="textinput">&nbsp;</label>
-		  <div class="col-md-4">
+		<div>
+	      <label class="control-label" for="textinput">&nbsp;</label>
+		  <div class="center">
 		    <input id="submit" name="submit" type="submit" class="btn btn-primary">
-		  </div>
+			</div>
+			<a class="center" href="/User/login">login</a>
 		</div>
 	</div>
 </form>
