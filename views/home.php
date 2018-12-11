ee9e6ca1439a26585d596ff18875328b774e5f2d
Shojin Sensei <nik.seger01@gmail.com>
2018-12-11 11:33:32 +0100

    designed Home view

--- views/home.php
+++ views/home.php
@@ -1,1 +1,33 @@
 <?php require_once('../views/header.php'); ?>
+
+<html>
+    <head>
+    </head>
+    <body>
+        <div id="home">
+            <a href="/Fight"><img id="fight" src="/images/main-symbol.jpg" alt="CRASH SOME NUTS"></a>
+            <div>
+            <div id="player_stats">
+                <div class="stat_block">
+                    Playername
+                </div>
+                <div class="stat_block">
+                    <div class="player_stats">
+                        Games
+                    </div>
+                    <div class="player_stats">
+                        Wins
+                    </div>
+                    <div class="player_stats">
+                        Win Ratio
+                    </div>
+                </div>
+                
+            </div>
+            <div id="fighter_info">
+                <a href="/Fighter"><img id="fighter_pic" src="/images/tank.jpg" alt="much edit such fighter"></a>
+            </div>
+            </div>
+        </div>
+    </body>
+</html>
