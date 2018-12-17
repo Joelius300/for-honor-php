5cf0a2686d0542954776379e353a5c3f61cd1265
Seger Nik <bsegen@bbc-be.local>
2018-12-17 10:23:37 +0100

    style home view

--- views/home.php
+++ views/home.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <div id="home">
-            <a href="/Fight"><img id="fight" src="/images/main-symbol.jpg" alt="CRASH SOME NUTS"></a>
+            <a class='home_pics' href="/Fight"><img id="fight" src="/images/main-symbol.jpg" alt="CRASH SOME NUTS"></a>
             <div>
             <div id="player_stats">
                 <div class="stat_block">
@@ -34,7 +34,7 @@
                 
             </div>
             <div id="fighter_info">
-                <a href="/Fighter"><img id="fighter_pic" src="/images/edit_fighter.png" alt="much edit such fighter"></a>
+                <a class='home_pics' href="/Fighter"><img id="fighter_pic" src="/images/edit_fighter.png" alt="much edit such fighter"></a>
             </div>
             </div>
         </div>
